8408ad6e4024f6fa573eafe6d70314da9c074748
inthrakumar <inthrak04@gmail.com>
2024-06-13 18:23:58 +0530

    Add feature to keyboard component

--- src/components/KeyBoard.tsx
+++ src/components/KeyBoard.tsx
@@ -1,11 +1,69 @@
-import React from 'react'
-
-function KeyBoard() {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useState } from 'react'
+import "../css/keyboard.css"
+type KeyboardProps = {
+    disabled?: boolean
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter: string) => void
 }
+const KEYS: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+function KeyBoard({
+    activeLetters,
+    inactiveLetters,
+    addGuessedLetter,
+    disabled = false,
+}: KeyboardProps) {
+    return (
+        <div className='keyboard'>
+            
+
+
+
+            {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`.btn ${isActive ? "active" : "inactive"} ${
+              isInactive ? "inactive" : "active"
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        )
+      })}
+                </div >
+                )
+            }
 
 export default KeyBoard
